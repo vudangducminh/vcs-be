ba7657ca6950362f14fc758c2e35105a69556cf9
vudangducminh <vudangducminh@gmail.com>
2025-03-11 17:15:37 +0700

    hello

---
+++ go-fundamental/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"go-fundamental/function"
+)
+
+func main() {
+	var a int = 1
+	fmt.Println("Add 2 to a: ", function.Add(a, 2))
+	fmt.Println("Multiply 3 numbers: ", function.Multiply(7, 2, 7))
+	fmt.Println("Multiply 4 numbers: ", function.Multiply(2, 3, 5, 7))
+	fmt.Println("Multiply 5 numbers: ", function.Multiply(6, 9, 4, 2, 0))
+	val, err := function.Divide(5, 0)
+	fmt.Println("Divide by zero: ", val, " ", err)
+	val, err = function.Divide(10, 3)
+	fmt.Println("Normal division: ", val, " ", err)
+}
