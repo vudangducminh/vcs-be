f928057dd6b983f25609183a306e2a862a64b05e
vudangducminh <vudangducminh@gmail.com>
2025-03-18 18:24:01 +0700

    udate multithreading

--- multithreading/test.go
+++ multithreading/test.go
@@ -2,49 +2,78 @@ package multithreading
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 var cakes []int
 var pancakeUsed = 0
+var mutex sync.Mutex
 
-func MakingCakes(id int, data chan int) {
+func MakingCakes(id int, data chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for cakeID := range data {
+		time.Sleep(2 * time.Second) // Significant amount of time to make a pancake
+		mutex.Lock()
 		fmt.Printf("Worker %d: Created pancake %d\n", id+1, cakeID+1)
 		cakes = append(cakes, cakeID+1)
-		time.Sleep(time.Second)
+		fmt.Println(cakes)
+		mutex.Unlock()
 	}
 }
 
-func ServingCakes(id int, data chan int) {
-	if len(cakes) == 0 {
-		fmt.Printf("Guest %d: Waiting for more pancakes :(\n", id+1)
-		return
+func ServingCakes(id int, data chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for cakeID := range data {
+		mutex.Lock()
+		pancakeUsed++
+		fmt.Printf("Guest %d: Used pancake %d\n", id+1, cakeID)
+		fmt.Println("Pancake used:", pancakeUsed)
+		mutex.Unlock()
+		time.Sleep(1 * time.Second) // Time taken to use a pancake
 	}
-	pancakeUsed++
-	fmt.Printf("Guest %d: Used pancake %d\n", id+1, cakes[len(cakes)-1])
-	cakes = cakes[:len(cakes)-1]
-	time.Sleep(time.Second)
 }
 
-func Func(data chan int) {
+func Func(data chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for pancakeUsed < 10 {
-		for i := 0; i < 3; i++ {
-			go ServingCakes(i, data)
-			time.Sleep(50 * time.Millisecond)
+		mutex.Lock()
+		if len(cakes) == 0 {
+			fmt.Printf("Waiting for more pancakes :(\n")
+			mutex.Unlock()
+			time.Sleep(time.Second)
+		} else {
+			cakeID := cakes[len(cakes)-1]
+			cakes = cakes[:len(cakes)-1]
+			mutex.Unlock()
+			data <- cakeID
 		}
-		time.Sleep(time.Second)
+	}
+	close(data)
+}
+
+func Test() {
+	var wg sync.WaitGroup
+	channel := make(chan int)
+	channel2 := make(chan int)
+
+	wg.Add(1)
+	go Func(channel2, &wg)
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go MakingCakes(i, channel, &wg)
 	}
 
-}
-func Test() {
-	channel := make(chan int)
-	go Func(channel)
 	for i := 0; i < 3; i++ {
-		go MakingCakes(i, channel)
+		wg.Add(1)
+		go ServingCakes(i, channel2, &wg)
 	}
+
 	for i := 0; i < 10; i++ {
 		channel <- i
 	}
+
 	close(channel)
+	wg.Wait()
 }
