386a278dc12995efef0150293c23466ea3c734c7
vudangducminh <vudangducminh@gmail.com>
2025-08-28 14:59:01 +0700

    Rename directory

---
+++ sms/service/user_service/login.go
@@ -0,0 +1,77 @@
+package users_handler
+
+import (
+	"log"
+	"net/http"
+	"sms/auth"
+	_ "sms/docs"
+	"sms/object"
+
+	redis_query "sms/server/database/cache/redis/query"
+	posgresql_query "sms/server/database/postgresql/query"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @Tags         Users
+// @Summary      Handle user login
+// @Description  Handle user login by validating credentials and generating a JWT token
+// @Accept       json
+// @Produce      json
+// @Param        request body object.LoginRequest true "Login request"
+// @Success      200 {object} object.LoginSuccessResponse "Login successful"
+// @Failure      400 {object} object.LoginBadRequestResponse "Invalid request body"
+// @Failure      401 {object} object.LoginUnauthorizedResponse "Invalid credentials"
+// @Failure      500 {object} object.LoginInternalServerErrorResponse "Error generating token
+// @Router       /users/login [post]
+func HandleLogin(c *gin.Context) {
+	var req object.LoginRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	log.Println("Username:", req.Username)
+	log.Println("Password:", req.Password)
+	if req.Username == "" || req.Password == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Username and password are required",
+			"error":   "Invalid credentials",
+		})
+		return
+	}
+
+	storedPassword := posgresql_query.GetAccountPasswordByUsername(req.Username)
+	if storedPassword != req.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Invalid username or password",
+			"error":   "Invalid credentials",
+		})
+		return
+	}
+
+	// Generate JWT token before redirecting the user
+	tokenString, err := auth.GenerateJWT(req.Username, req.Password)
+	if err != nil {
+		// handle error
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Error generating token",
+			"error":   "Error generating token",
+		})
+		return
+	}
+	// Set the cookie in the response
+
+	if redis_query.SaveJWTToken(tokenString, 1800) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Login successful",
+			"token":   tokenString,
+		})
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error saving JWT token",
+			"error":   "Error saving JWT token",
+		})
+	}
+}
